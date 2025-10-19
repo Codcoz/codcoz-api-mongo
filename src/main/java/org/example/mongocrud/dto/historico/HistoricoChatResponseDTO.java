ce9be9c45510ae01ad91a14cb276d7eda814d5e9
Gustavo De Longhi <gustavodelonghi@gmail.com>
2025-10-19 16:00:53 -0300

    fix(historico): removing useless comments

--- src/main/java/org/example/mongocrud/dto/historico/HistoricoChatResponseDTO.java
+++ src/main/java/org/example/mongocrud/dto/historico/HistoricoChatResponseDTO.java
@@ -24,26 +24,3 @@ eventuais modificações no model (caso seja dados sensíveis, por exemplo), o
 que será retornado ao usuário permanecerá a mesma coisa (pois a alteração no
 model, em caso incremental, não alterará o ResponseDTO)
 */
-
-
-
-/*
-*
-* package org.example.mongocrud.dto.empresa;
-
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-import org.springframework.data.annotation.Id;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-@ToString
-public class EmpresaResponseDTO {
-    private String id;
-    private String nome;
-    private String sigla;
-}*/
