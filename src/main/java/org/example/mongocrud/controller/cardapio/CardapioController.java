2c7ddf0b758b5f654c6afb5658e30e0edb205092
Gustavo De Longhi <gustavodelonghi@gmail.com>
2025-10-22 10:47:40 -0300

    feat: menu controller

--- src/main/java/org/example/mongocrud/controller/cardapio/CardapioController.java
+++ src/main/java/org/example/mongocrud/controller/cardapio/CardapioController.java
@@ -6,10 +6,12 @@ import org.example.mongocrud.dto.cardapio.CardapioResponseDTO;
 import org.example.mongocrud.service.CardapioService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RequiredArgsConstructor
+@RestController
 @RequestMapping("api/v1/cardapio")
 public class CardapioController implements CardapioApi{
     private final CardapioService cardapioService;
